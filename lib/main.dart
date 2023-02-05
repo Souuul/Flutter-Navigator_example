3368f1f5cd767383c57901bee2cfdea6410c8fca
Sol Han <hans53400@naver.com>
2023-02-05 13:02:37 +0900

    make navigator example

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SubPage Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/' : (context) => FirstPage(),
+        '/second' : (context) => SecondPage(),
+      },
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FirstPage();
+}
+
+class _FirstPage extends State<FirstPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Sub Page Main'),
+        ),
+        body: Container(
+          child: Center(
+            child: Text('첫번째 페이지'),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).pushNamed('/second');
+          },
+          child: Icon(Icons.add),
+        ));
+  }
+}
+
+class SecondPage extends StatelessWidget {
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Second Page'),
+        ),
+        body : Container(
+          child: Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('돌아가기'),
+            ),
+          ),
+        )
+    );
+  }
+
+}
